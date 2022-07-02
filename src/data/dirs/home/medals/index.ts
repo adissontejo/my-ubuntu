cf94a1542827c1e0a19e1173bbfa82d229ee74b2
adissontejo <adissonmprtejo@gmail.com>
2022-07-02 00:53:23 -0300

    Add more medals

--- src/data/dirs/home/medals/index.ts
+++ src/data/dirs/home/medals/index.ts
@@ -19,9 +19,39 @@ export const medals: Dir = {
       src: '/icons/bronze-medal.svg',
     },
     {
+      name: 'ORI Maratona - 2019',
+      type: 'png',
+      src: '/icons/gold-medal.svg',
+    },
+    {
+      name: 'ORI Olimpíada - 2019',
+      type: 'png',
+      src: '/icons/gold-medal.svg',
+    },
+    {
+      name: 'OPI Maratona - 2019',
+      type: 'png',
+      src: '/icons/gold-medal.svg',
+    },
+    {
+      name: 'OBR Teórica - 2019',
+      type: 'png',
+      src: '/icons/bronze-medal.svg',
+    },
+    {
+      name: 'OBR Prática Nacional - 2019',
+      type: 'png',
+      src: '/icons/award.svg',
+    },
+    {
       name: 'OBR Prática Estadual - 2019',
       type: 'png',
       src: '/icons/silver-medal.svg',
+    },
+    {
+      name: 'OBR Prática Regional - 2019',
+      type: 'png',
+      src: '/icons/gold-medal.svg',
     },
   ],
 };
